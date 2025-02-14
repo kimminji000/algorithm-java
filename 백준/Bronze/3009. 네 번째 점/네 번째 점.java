d3c6ac37b85711358220e748325b728012e20c35
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-15 01:08:31 +0900

    [Bronze III] Title: 네 번째 점, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int[] x = new int[3];
+		int[] y = new int[3];
+
+		for (int i = 0; i < 3; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
+		}
+
+		if (x[0] == x[1]) {
+			sb.append(x[2]).append(" ");
+		} else if (x[0] == x[2]) {
+			sb.append(x[1]).append(" ");
+		} else {
+			sb.append(x[0]).append(" ");
+		}
+
+		if (y[0] == y[1]) {
+			sb.append(y[2]);
+		} else if (y[0] == y[2]) {
+			sb.append(y[1]);
+		} else {
+			sb.append(y[0]);
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
