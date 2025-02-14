c8f4a16289ddaf1ec9dbc1be14ea661cbe708e19
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-14 23:32:00 +0900

    [Bronze III] Title: 공 넣기, Time: 68 ms, Memory: 11912 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] basket = new int[n + 1];
+
+		for (int a = 0; a < m; a++) {
+			st = new StringTokenizer(br.readLine());
+
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+
+			for (int b = i; b <= j; b++) {
+				basket[b] = k;
+			}
+		}
+
+		for (int a = 1; a <= n; a++) {
+			sb.append(basket[a]).append(" ");
+
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
