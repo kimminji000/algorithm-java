86bb97a32e958eceb8e797375d41dc168d8077d7
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-15 00:54:23 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 12036 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] card = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(card);
+
+		int max = 0;
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {
+					if (card[i] + card[j] + card[k] > m) {
+						break;
+					}
+					if (card[i] + card[j] + card[k] > max) {
+						max = card[i] + card[j] + card[k];
+					}
+				}
+			}
+		}
+
+		sb.append(max);
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
