4983a40b28fd2125cb8fd7d309c1f7963e6a3663
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-14 23:32:48 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("         ,r'\"7").append("\n");
+		sb.append("r`-_   ,'  ,/").append("\n");
+		sb.append(" \\. \". L_r'").append("\n");
+		sb.append("   `~\\/").append("\n");
+		sb.append("      |").append("\n");
+		sb.append("      |").append("\n");
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
