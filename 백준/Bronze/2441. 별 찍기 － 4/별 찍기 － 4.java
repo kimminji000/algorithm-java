7d5dfcd488561d91f85cb99f1da47a0d8b4cbb40
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-14 23:13:05 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 72 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				sb.append(" ");
+			}
+			for (int j = n - i; j > 0; j--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+
+//		for (int i = 0; i < n; i++) {
+//			System.out.println(" ".repeat(i) + "*".repeat(n-i));
+//		}
+	}
+}
