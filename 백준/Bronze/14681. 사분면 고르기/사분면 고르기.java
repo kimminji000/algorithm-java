99155ce57ac6c95ba2908646fa425c1728af045a
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-14 23:30:00 +0900

    [Bronze V] Title: 사분면 고르기, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+
+		if (x > 0 && y > 0) {
+			sb.append(1);
+		} else if (x < 0 && y > 0) {
+			sb.append(2);
+		} else if (x < 0 && y < 0) {
+			sb.append(3);
+		} else {
+			sb.append(4);
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
