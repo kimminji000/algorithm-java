3ab2f1e6825288df498a62ebaa24605df86f8f4c
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-14 23:13:29 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 80 ms, Memory: 11916 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			for (int j = 1; j <= 2 * (n - i); j++) {
+				sb.append(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		for (int i = n - 1; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			for (int j = 1; j <= 2 * (n - i); j++) {
+				sb.append(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+
+//		for (int i = 1; i <= n; i++) {
+//			System.out.println("*".repeat(i) + " ".repeat(2 * (n - i)) + "*".repeat(i));
+//		}
+//
+//		for (int i = n - 1; i >= 1; i--) {
+//			System.out.println("*".repeat(i) + " ".repeat(2 * (n - i)) + "*".repeat(i));
+//		}
+	}
+}
