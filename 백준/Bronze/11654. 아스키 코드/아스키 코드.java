a3c45ff4cb48496c032033449543953a0e04fe5e
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-14 23:31:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		char text = br.readLine().charAt(0);
+
+		sb.append((int) (text));
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
