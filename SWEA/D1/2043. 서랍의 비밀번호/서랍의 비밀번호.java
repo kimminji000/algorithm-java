6e6dfebc1b02bfbc37c22c206ed18bfbd25e594d
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-19 11:40:46 +0900

    [D1] Title: 서랍의 비밀번호, Time: 96 ms, Memory: 24,064 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int p = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		sb.append(p - k + 1).append("\n");
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
