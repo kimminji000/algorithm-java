67678a05a01dde780699184e02e00ab6feef7983
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-19 11:36:17 +0900

    [D1] Title: N줄덧셈, Time: 79 ms, Memory: 24,320 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+
+		sb.append(sum);
+
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
