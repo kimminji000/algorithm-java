934bac3818ea7ad626a8843ff874e0b22f45f360
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-20 12:44:33 +0900

    [D1] Title: 최대수 구하기, Time: 79 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -4,34 +4,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int t = Integer.parseInt(br.readLine());
-
-		for (int i = 0; i < t; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			int max = Integer.parseInt(st.nextToken());
-
-			for (int j = 1; j < 10; j++) {
-				int num = Integer.parseInt(st.nextToken());
-
-				if (num > max) {
-					max = num;
-				}
-			}
-
-			sb.append("#").append(i + 1).append(" ").append(max).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		br.close();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+ 
+        int t = Integer.parseInt(br.readLine());
+ 
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+            int max = Integer.parseInt(st.nextToken());
+ 
+            for (int j = 1; j < 10; j++) {
+                int num = Integer.parseInt(st.nextToken());
+ 
+                if (num > max) {
+                    max = num;
+                }
+            }
+ 
+            sb.append("#").append(i + 1).append(" ").append(max).append("\n");
+        }
+ 
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
