e40fd44bb60031a0743e554b45ce2803070316c6
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-02-20 12:29:17 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 76 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -4,6 +4,21 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 
 public class Solution {
+	public static boolean popCheck(ArrayDeque<Integer> stack, int x) {
+		boolean flag = true;
+
+		if (stack.isEmpty())
+			flag = false;
+		else {
+			if (stack.peek() == x)
+				stack.pop();
+			else
+				flag = false;
+		}
+
+		return flag;
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -21,74 +36,41 @@ public class Solution {
 			boolean flag = true;
 
 			for (int i = 0; i < length; i++) {
-				if (str.charAt(i) == '(') {
+				if (str.charAt(i) == '(')
 					stack.push(0);
-				} else if (str.charAt(i) == ')') {
-					if (stack.isEmpty()) {
-						flag = false;
+				else if (str.charAt(i) == ')') {
+					flag = popCheck(stack, 0);
+					if (!flag)
 						break;
-					} else {
-						if (stack.peek() == 0) {
-							stack.pop();
-						} else {
-							flag = false;
-							break;
-						}
-					}
-				} else if (str.charAt(i) == '[') {
+				} else if (str.charAt(i) == '[')
 					stack.push(1);
-				} else if (str.charAt(i) == ']') {
-					if (stack.isEmpty()) {
-						flag = false;
+				else if (str.charAt(i) == ']') {
+					flag = popCheck(stack, 1);
+					if (!flag)
 						break;
-					} else {
-						if (stack.peek() == 1) {
-							stack.pop();
-						} else {
-							flag = false;
-							break;
-						}
-					}
-				} else if (str.charAt(i) == '{') {
+				} else if (str.charAt(i) == '{')
 					stack.push(2);
-				} else if (str.charAt(i) == '}') {
-					if (stack.isEmpty()) {
-						flag = false;
+				else if (str.charAt(i) == '}') {
+					flag = popCheck(stack, 2);
+					if (!flag)
 						break;
-					} else {
-						if (stack.peek() == 2) {
-							stack.pop();
-						} else {
-							flag = false;
-							break;
-						}
-					}
-				} else if (str.charAt(i) == '<') {
+				} else if (str.charAt(i) == '<')
 					stack.push(3);
-				} else if (str.charAt(i) == '>') {
-					if (stack.isEmpty()) {
-						flag = false;
+				else if (str.charAt(i) == '>') {
+					flag = popCheck(stack, 3);
+					if (!flag)
 						break;
-					} else {
-						if (stack.peek() == 3) {
-							stack.pop();
-						} else {
-							flag = false;
-							break;
-						}
-					}
 				}
 			}
 
-			if (!stack.isEmpty()) {
+			if (!stack.isEmpty())
 				flag = false;
-			}
 
-			if (flag) {
+			if (flag)
 				sb.append("#").append(t).append(" ").append("1").append("\n");
-			} else {
+			else
 				sb.append("#").append(t).append(" ").append("0").append("\n");
-			}
+
 		}
 
 		System.out.println(sb.toString());
