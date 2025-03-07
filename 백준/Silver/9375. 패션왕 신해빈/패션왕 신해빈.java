909ab88fd9dddac4bd74ee8c8ec5e9c0432afd5e
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-03-07 22:35:58 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 68 ms, Memory: 11840 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+
+			Map<String, Integer> category = new LinkedHashMap<>();
+
+			for (int j = 0; j < n; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+
+				st.nextToken(); // name
+				String cate = st.nextToken();
+
+				if (category.containsKey(cate)) {
+					category.put(cate, category.get(cate) + 1);
+				} else {
+					category.put(cate, 1);
+				}
+			}
+
+			int cnt = 1;
+
+			for (int j : category.values()) {
+				cnt *= (j + 1);
+			}
+
+			sb.append(cnt - 1).append("\n");
+
+		}
+
+		System.out.println(sb.toString());
+
+		br.close();
+	}
+}
