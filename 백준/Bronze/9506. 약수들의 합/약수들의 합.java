5a11f20e7c374813ef55da9f7ad864049d53e880
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-03-11 11:48:12 +0900

    [Bronze I] Title: 약수들의 합, Time: 68 ms, Memory: 11764 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -13,24 +11,18 @@ public class Main {
 		while (n != -1) {
 			StringBuilder sb = new StringBuilder();
 
-			List<Integer> nums = new ArrayList<>();
-
 			int sum = 0;
+
+			sb.append(n).append(" = ");
 
 			for (int i = 1; i <= n / 2; i++) {
 				if (n % i == 0) {
-					nums.add(i);
 					sum += i;
+					sb.append(i).append(" + ");
 				}
 			}
 
 			if (sum == n) {
-				sb.append(n).append(" = ");
-
-				for (int i : nums) {
-					sb.append(i).append(" + ");
-				}
-
 				System.out.println(sb.substring(0, sb.length() - 2).toString());
 			} else {
 				System.out.println(n + " is NOT perfect.");
