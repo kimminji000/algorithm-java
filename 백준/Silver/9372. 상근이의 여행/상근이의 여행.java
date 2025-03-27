68c1be09dc45faa5b38a977e27e71e51c97c66bc
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-03-27 11:47:46 +0900

    [Silver IV] Title: 상근이의 여행, Time: 164 ms, Memory: 23108 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 0; tc < t; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			for (int i = 0; i < m; i++) {
+				br.readLine();
+			}
+
+			System.out.println(n - 1 + " ");
+		}
+	}
+}
