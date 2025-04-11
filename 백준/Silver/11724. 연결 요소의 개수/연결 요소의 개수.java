f6cc234fbda41536aeb99dd6f967aee77a8080ae
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-04-11 17:20:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 560 ms, Memory: 145232 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -10,9 +8,22 @@ public class Main {
 	static ArrayList<Integer>[] graph;
 	static boolean visited[];
 
+	private static void dfs(int v) {
+		if (visited[v]) {
+			return;
+		}
+
+		visited[v] = true;
+
+		for (int i : graph[v]) {
+			if (!visited[i]) {
+				dfs(i);
+			}
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -44,24 +55,6 @@ public class Main {
 			}
 		}
 
-		bw.write(Integer.toString(cnt));
-
-		bw.flush();
-		br.close();
-		bw.close();
-	}
-
-	private static void dfs(int v) {
-		if (visited[v]) {
-			return;
-		}
-
-		visited[v] = true;
-
-		for (int i : graph[v]) {
-			if (!visited[i]) {
-				dfs(i);
-			}
-		}
+		System.out.println(cnt);
 	}
 }
