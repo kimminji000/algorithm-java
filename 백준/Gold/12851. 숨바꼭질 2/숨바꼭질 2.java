19dce6d680ce0cc1eaa3874c462758291764d5cd
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-04-29 09:42:54 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 116 ms, Memory: 22712 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -6,41 +6,48 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int[] time;
-	private static int minTime;
-	private static int cnt;
+	static int minTime;
+	static int cnt;
 
-	private static void bfs(int n, int k) {
+	private static int[] bfs(int n, int k) {
 		Queue<Integer> queue = new ArrayDeque<>();
 		queue.offer(n);
+		int[] time = new int[100001];
 		time[n] = 1;
+
+		minTime = Integer.MAX_VALUE;
+		cnt = 0;
 
 		while (!queue.isEmpty()) {
 			int now = queue.poll();
 
 			if (now == k) {
-				if (minTime > time[now]) {
-					minTime = time[now];
-					cnt = 1;
-				} else if (minTime == time[now]) {
-					cnt++;
-				}
+				minTime = time[now];
+				cnt++;
 			}
 
 			if (minTime < time[now]) {
-				return;
+				return new int[] { minTime, cnt };
+
 			}
 
 			int[] nextPos = { now - 1, now + 1, now * 2 };
+
 			for (int next : nextPos) {
 				if (next >= 0 && next <= 100000) {
-					if (time[next] == 0 || time[next] == time[now] + 1) {
+					if (time[next] == time[now] + 1) { // 이전에 최단거리로 방문한 적 있음
+						queue.offer(next);
+					}
+
+					if (time[next] == 0) { // 방문한 적 없음
 						queue.offer(next);
 						time[next] = time[now] + 1;
 					}
 				}
 			}
 		}
+
+		return new int[] { minTime, cnt };
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -51,11 +58,10 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		time = new int[100001];
-		minTime = Integer.MAX_VALUE;
-		cnt = 0;
+		int[] result = bfs(n, k);
 
-		bfs(n, k);
+		int minTime = result[0];
+		int cnt = result[1];
 
 		System.out.println(minTime - 1);
 		System.out.println(cnt);
