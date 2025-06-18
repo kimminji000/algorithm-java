c713848ef6e061cf7cabd090d73f8623bd5c6022
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-06-18 23:20:31 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 60 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int m = Integer.parseInt(st.nextToken());
+			int f = Integer.parseInt(st.nextToken());
+
+			if (m == 0 && f == 0) {
+				break;
+			}
+
+			System.out.println(m + f);
+		}
+	}
+}
