871cf79ac003f3e7b5892e1343712245e069deed
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-06-24 23:36:09 +0900

    [Bronze IV] Title: 영수증, Time: 72 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int price = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < 9; i++) {
+			price -= Integer.parseInt(br.readLine());
+		}
+
+		System.out.println(price);
+	}
+}
