015b4f2100e409c445768531fe3a792d8330e9b6
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-06-25 23:54:23 +0900

    [Bronze IV] Title: !밀비 급일, Time: 60 ms, Memory: 11336 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			String str = br.readLine();
+
+			if (str.equals("END")) {
+				break;
+			}
+
+			for (int i = str.length(); i > 0; i--) {
+				sb.append(str.charAt(i - 1));
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
