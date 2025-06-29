aa0567517a3c6d82a1a90381e99e04ff71ea15c0
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-06-29 23:26:16 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 64 ms, Memory: 11372 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			String str = br.readLine();
+
+			if (str.equals("# 0 0")) {
+				break;
+			}
+
+			StringTokenizer st = new StringTokenizer(str);
+
+			String name = st.nextToken();
+			int age = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+
+			if (age > 17 || weight >= 80) {
+				System.out.println(name + " Senior");
+			} else {
+				System.out.println(name + " Junior");
+			}
+		}
+	}
+}
