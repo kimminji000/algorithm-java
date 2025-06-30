ebb4c78016b128ad32d9637505c112f01c932e99
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-07-01 00:03:46 +0900

    [Bronze IV] Title: 특별한 날, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int month = Integer.parseInt(br.readLine());
+		int day = Integer.parseInt(br.readLine());
+
+		if (month < 2) {
+			System.out.println("Before");
+		} else if (month > 2) {
+			System.out.println("After");
+		} else {
+			if (day < 18) {
+				System.out.println("Before");
+			} else if (day > 18) {
+				System.out.println("After");
+			} else {
+				System.out.println("Special");
+			}
+		}
+	}
+}
