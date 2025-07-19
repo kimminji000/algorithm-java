91ba795c718a70394a7577895688dfa4b051040a
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-07-20 00:10:30 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 68 ms, Memory: 11996 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int sum = 0;
+
+		while (true) {
+			int n = Integer.parseInt(br.readLine());
+
+			if (n == -1) {
+				break;
+			}
+
+			sum += n;
+		}
+
+		System.out.println(sum);
+	}
+}
