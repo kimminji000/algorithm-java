127a810432ce37b0981b491e3d95f3ce91672667
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-26 17:01:34 +0900

    [Bronze II] Title: Strfry, Time: 248 ms, Memory: 21116 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -1,33 +1,35 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
 		int n = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 
 			String str1 = st.nextToken();
 			String str2 = st.nextToken();
 
-			boolean flag = false;
+			boolean flag = true;
 
-			if (str1.length() == str2.length()) {
-				flag = true;
+			if (str1.length() != str2.length()) {
+				flag = false;
+			}
 
+			if (flag) {
 				int[] alpha1 = new int[26];
 				int[] alpha2 = new int[26];
 
 				for (int j = 0; j < str1.length(); j++) {
 					alpha1[str1.charAt(j) - 'a']++;
+				}
+				for (int j = 0; j < str2.length(); j++) {
 					alpha2[str2.charAt(j) - 'a']++;
 				}
 
@@ -40,13 +42,10 @@ public class Main {
 			}
 
 			if (flag) {
-				bw.write("Possible\n");
+				System.out.println("Possible");
 			} else {
-				bw.write("Impossible\n");
+				System.out.println("Impossible");
 			}
 		}
-
-		bw.flush();
-		bw.close();
 	}
 }
