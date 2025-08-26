dd1eef39db3c136d06d56cd7b4c7210a3a349da6
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-26 15:46:14 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 11416 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,30 +1,26 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int a = Integer.parseInt(br.readLine());
 		int b = Integer.parseInt(br.readLine());
 		int c = Integer.parseInt(br.readLine());
-		String mul = Integer.toString(a * b * c);
 
-		int[] num = new int[10];
+		int num = a * b * c;
 
-		for (int i = 0; i < mul.length(); i++) {
-			num[(int) (mul.charAt(i) - '0')]++;
+		int[] cnt = new int[10];
+
+		while (num > 0) {
+			cnt[num % 10]++;
+			num /= 10;
 		}
 
-		for (int x : num) {
-			bw.write(x + "\n");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(cnt[i]);
 		}
-
-		bw.flush();
-		bw.close();
 	}
 }
