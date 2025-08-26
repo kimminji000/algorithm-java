1a9f8dfef56543db5fa9bb137b05a3d274ad2b3c
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-26 17:17:03 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,36 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String str1 = br.readLine();
 		String str2 = br.readLine();
 
-		int[] alpha = new int[26];
+		int[] alpha1 = new int[26];
+		int[] alpha2 = new int[26];
 
 		for (int i = 0; i < str1.length(); i++) {
-			alpha[str1.charAt(i) - 'a']++;
+			alpha1[str1.charAt(i) - 'a']++;
 		}
-
 		for (int i = 0; i < str2.length(); i++) {
-			alpha[str2.charAt(i) - 'a']--;
+			alpha2[str2.charAt(i) - 'a']++;
 		}
 
 		int cnt = 0;
+
 		for (int i = 0; i < 26; i++) {
-			cnt += Math.abs(alpha[i]);
+			cnt += Math.abs(alpha1[i] - alpha2[i]);
 		}
 
-		bw.write(Integer.toString(cnt));
-
-		bw.flush();
-		br.close();
-		bw.close();
+		System.out.println(cnt);
 	}
 }
