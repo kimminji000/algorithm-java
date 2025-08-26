f14d26a6ea34f1d4d0bdd31ece89a3f52828441a
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-26 16:32:23 +0900

    [Bronze V] Title: 개수 세기, Time: 68 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,21 +1,17 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
 
-		int[] num = new int[n];
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int[] num = new int[n];
 
 		for (int i = 0; i < n; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
@@ -23,20 +19,14 @@ public class Main {
 
 		int v = Integer.parseInt(br.readLine());
 
-		Arrays.sort(num);
-
 		int cnt = 0;
+
 		for (int i = 0; i < n; i++) {
 			if (num[i] == v) {
 				cnt++;
-			} else if (num[i] > v) {
-				break;
 			}
 		}
 
-		bw.write(Integer.toString(cnt));
-
-		bw.flush();
-		bw.close();
+		System.out.println(cnt);
 	}
 }
