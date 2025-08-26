2eea59579dc0864f4732979fe0e849dda2195496
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-26 16:41:58 +0900

    [Bronze II] Title: 방 배정, Time: 80 ms, Memory: 11956 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -1,61 +1,39 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 
-		int[] mNum = new int[7], wNum = new int[7];
+		int[] boy = new int[7];
+		int[] girl = new int[7];
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
 
-			if (Integer.parseInt(st.nextToken()) == 0) {
-				wNum[Integer.parseInt(st.nextToken())]++;
+			if (s == 0) {
+				girl[Integer.parseInt(st.nextToken())]++;
 			} else {
-				mNum[Integer.parseInt(st.nextToken())]++;
+				boy[Integer.parseInt(st.nextToken())]++;
 			}
 		}
 
 		int cnt = 0;
 
-		for (int i = 1; i <= 6; i++) {
-			if (wNum[i] == 0) {
-				continue;
-			} else if (wNum[i] % k == 0) {
-				cnt += wNum[i] / k;
-
-			} else {
-				cnt += wNum[i] / k + 1;
-			}
-
+		for (int i = 1; i < 7; i++) {
+			cnt += girl[i] % k == 0 ? girl[i] / k : girl[i] / k + 1;
 		}
-
-		for (int i = 1; i <= 6; i++) {
-			if (mNum[i] == 0) {
-				continue;
-			} else if (mNum[i] % k == 0) {
-				cnt += mNum[i] / k;
-
-			} else {
-				cnt += mNum[i] / k + 1;
-			}
-
+		for (int i = 1; i < 7; i++) {
+			cnt += boy[i] % k == 0 ? boy[i] / k : boy[i] / k + 1;
 		}
-
-		bw.write(Integer.toString(cnt));
-
-		bw.flush();
-		bw.close();
+		
+		System.out.println(cnt);
 	}
 }
