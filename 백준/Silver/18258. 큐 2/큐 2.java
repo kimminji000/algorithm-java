f340fc131ae4d24f2685f18d2d1978aeb31102a7
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-27 14:54:11 +0900

    [Silver IV] Title: 큐 2, Time: 856 ms, Memory: 323280 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,93 +1,88 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-class MyQueue {
-	int[] queue = new int[2000000];
-	int front = 0;
-	int back = 0;
-
-	public void push(int x) {
-		queue[back++] = x;
-	}
-
-	public int pop() {
-		if (empty() == 1) {
-			return -1;
-		}
-		return queue[front++];
-	}
-
-	public int size() {
-		return back - front;
-	}
-
-	public int empty() {
-		if (front == back) {
-			return 1;
-		}
-		return 0;
-	}
-
-	public int front() {
-		if (empty() == 1) {
-			return -1;
-		}
-		return queue[front];
-	}
-
-	public int back() {
-		if (empty() == 1) {
-			return -1;
-		}
-		return queue[back - 1];
-	}
-}
-
 public class Main {
+	static class MyQueue {
+		int front = 0;
+		int rear = 0;
+		int[] arr = new int[2000000];
+
+		void push(int x) {
+			arr[rear++] = x;
+		}
+
+		int pop() {
+			if (empty() == 1) {
+				return -1;
+			}
+			return arr[front++];
+		}
+
+		int size() {
+			return rear - front;
+		}
+
+		int empty() {
+			if (front == rear) {
+				return 1;
+			}
+			return 0;
+		}
+
+		int front() {
+			if (empty() == 1) {
+				return -1;
+			}
+			return arr[front];
+		}
+
+		int back() {
+			if (empty() == 1) {
+				return -1;
+			}
+			return arr[rear - 1];
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
 		int n = Integer.parseInt(br.readLine());
 
-		MyQueue myQueue = new MyQueue();
+		MyQueue queue = new MyQueue();
 
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 
 			String str = st.nextToken();
 
 			switch (str) {
 			case "push":
-				int num = Integer.parseInt(st.nextToken());
-				myQueue.push(num);
+				int x = Integer.parseInt(st.nextToken());
+				queue.push(x);
 				break;
 			case "pop":
-				sb.append(myQueue.pop()).append("\n");
+				sb.append(queue.pop()).append("\n");
 				break;
 			case "size":
-				sb.append(myQueue.size()).append("\n");
+				sb.append(queue.size()).append("\n");
 				break;
 			case "empty":
-				sb.append(myQueue.empty()).append("\n");
+				sb.append(queue.empty()).append("\n");
 				break;
 			case "front":
-				sb.append(myQueue.front()).append("\n");
+				sb.append(queue.front()).append("\n");
 				break;
 			case "back":
-				sb.append(myQueue.back()).append("\n");
+				sb.append(queue.back()).append("\n");
 				break;
 			}
 		}
 
-		bw.write(sb.toString());
-		bw.flush();
-		br.close();
-		bw.close();
+		System.out.println(sb.toString());
 	}
 }
