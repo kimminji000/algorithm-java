60169df0e1b878966b7ae0f88e647ab62f8d1368
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-27 10:01:52 +0900

    [Silver IV] Title: 큐, Time: 272 ms, Memory: 17176 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,74 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class MyQueue {
-		int front = 0;
-		int rear = 0;
-		int[] arr = new int[10000];
-
-		void push(int x) {
-			arr[rear++] = x;
-		}
-
-		int pop() {
-			if (rear == front)
-				return -1;
-			else
-				return arr[front++];
-		}
-
-		int size() {
-			return rear - front;
-		}
-
-		int empty() {
-			if (rear == front)
-				return 1;
-			else
-				return 0;
-		}
-
-		int front() {
-			if (rear == front)
-				return -1;
-			else
-				return arr[front];
-		}
-
-		int back() {
-			if (rear == front)
-				return -1;
-			else
-				return arr[rear - 1];
-		}
-	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		MyQueue myQueue = new MyQueue();
+		StringTokenizer st;
 
 		int n = Integer.parseInt(br.readLine());
 
+		Deque<Integer> queue = new ArrayDeque<>();
+
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
+
 			String str = st.nextToken();
 
-			if (str.equals("push"))
-				myQueue.push(Integer.parseInt(st.nextToken()));
-			else if (str.equals("pop"))
-				System.out.println(myQueue.pop());
-			else if (str.equals("size"))
-				System.out.println(myQueue.size());
-			else if (str.equals("empty"))
-				System.out.println(myQueue.empty());
-			else if (str.equals("front"))
-				System.out.println(myQueue.front());
-			else if (str.equals("back"))
-				System.out.println(myQueue.back());
+			switch (str) {
+			case "push":
+				Integer x = Integer.parseInt(st.nextToken());
+				queue.offer(x);
+				break;
+			case "pop":
+				if (queue.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(queue.poll());
+				}
+				break;
+			case "size":
+				System.out.println(queue.size());
+				break;
+			case "empty":
+				if (queue.isEmpty()) {
+					System.out.println("1");
+				} else {
+					System.out.println("0");
+				}
+				break;
+			case "front":
+				if (queue.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(queue.peekFirst());
+				}
+				break;
+			case "back":
+				if (queue.isEmpty()) {
+					System.out.println("-1");
+				} else {
+					System.out.println(queue.peekLast());
+				}
+				break;
+			}
 		}
 	}
 }
