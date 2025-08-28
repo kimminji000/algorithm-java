b271cac982153526ac3618254d3cc7251297f523
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-08-28 20:48:06 +0900

    [Gold V] Title: 탑, Time: 528 ms, Memory: 111516 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,41 +6,29 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static class Tower {
-		int index;
-		int height;
-
-		public Tower(int index, int height) {
-			super();
-			this.index = index;
-			this.height = height;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		int n = Integer.parseInt(br.readLine());
 
-		Tower[] towers = new Tower[n + 1];
+		int[] towers = new int[n + 1];
 		int[] receivers = new int[n + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		for (int i = 1; i <= n; i++) {
-			towers[i] = new Tower(i, Integer.parseInt(st.nextToken()));
+			towers[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Deque<Tower> stack = new ArrayDeque<>();
-		stack.push(towers[n]);
+		Deque<Integer> stack = new ArrayDeque<>();
+		stack.push(n);
 
 		for (int i = n - 1; i > 0; i--) {
-			while (!stack.isEmpty() && towers[i].height >= stack.peek().height) {
-				Tower tower = stack.pop();
-				receivers[tower.index] = i;
+			while (!stack.isEmpty() && towers[i] >= towers[stack.peek()]) {
+				receivers[stack.pop()] = i;
 			}
-			stack.push(towers[i]);
+			stack.push(i);
 		}
 
 		for (int i = 1; i <= n; i++) {
