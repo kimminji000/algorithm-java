fc5a40ba588069e09ddb9f09542ec2630fa6c9df
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-09-21 03:21:51 +0900

    [level 0] Title: 조건 문자열, Time: 1.73 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -2,19 +2,37 @@ class Solution {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
         
-        if(ineq.equals(">")) {
-            if(eq.equals("=")) {
+        String str = ineq + eq;
+        
+        switch (str) {
+            case ">=":
                 answer = n >= m ? 1 : 0;
-            } else {
+                break;
+            case ">!":
                 answer = n > m ? 1 : 0;
-            }
-        } else {
-            if(eq.equals("=")) {
+                break;
+            case "<=":
                 answer = n <= m ? 1 : 0;
-            } else {
+                break;
+            case "<!":
                 answer = n < m ? 1 : 0;
-            }
+                break;
         }
+        
+        
+        // if(ineq.equals(">")) {
+        //     if(eq.equals("=")) {
+        //         answer = n >= m ? 1 : 0;
+        //     } else {
+        //         answer = n > m ? 1 : 0;
+        //     }
+        // } else {
+        //     if(eq.equals("=")) {
+        //         answer = n <= m ? 1 : 0;
+        //     } else {
+        //         answer = n < m ? 1 : 0;
+        //     }
+        // }
         
         return answer;
     }
