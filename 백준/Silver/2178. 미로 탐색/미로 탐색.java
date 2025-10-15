b5c8c9cbe905f4decb504db404a53f2d71fc49ae
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2025-10-15 16:02:49 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,70 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[][] map;
+	static boolean[][] visited;
+	static int n, m;
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
-	static boolean[][] visited;
-	static int[][] graph;
-	static int n, m;
+
+	private static int bfs(int x, int y) {
+		Queue<int[]> queue = new ArrayDeque<>();
+
+		queue.offer(new int[] { x, y });
+		visited[x][y] = true;
+
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nx = curr[0] + dx[i];
+				int ny = curr[1] + dy[i];
+
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+					continue;
+				}
+
+				if (map[nx][ny] == 1 && !visited[nx][ny]) {
+					visited[nx][ny] = true;
+					map[nx][ny] = map[curr[0]][curr[1]] + 1;
+					queue.offer(new int[] { nx, ny });
+
+					if (nx == n - 1 && ny == m - 1) {
+						return map[nx][ny];
+					}
+				}
+			}
+		}
+
+		return map[n - 1][m - 1];
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
+		map = new int[n][m];
 		visited = new boolean[n][m];
-		graph = new int[n][m];
 
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
+
 			for (int j = 0; j < m; j++) {
-				graph[i][j] = str.charAt(j) - '0';
+				map[i][j] = str.charAt(j) - '0';
 			}
 		}
 
-		bfs(0, 0);
+		int cnt = bfs(0, 0);
 
-		bw.write(Integer.toString(graph[n - 1][m - 1]));
-		bw.flush();
-		br.close();
-		bw.close();
-	}
-
-	private static void bfs(int x, int y) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] { x, y });
-		visited[x][y] = true;
-
-		while (!queue.isEmpty()) {
-			int now[] = queue.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int a = now[0] + dx[i];
-				int b = now[1] + dy[i];
-
-				if (a >= 0 && b >= 0 && a < n && b < m) {
-					if (graph[a][b] == 1 && visited[a][b] == false) {
-						visited[a][b] = true;
-						graph[a][b] = graph[now[0]][now[1]] + 1;
-						queue.add(new int[] { a, b });
-					}
-				}
-			}
-		}
+		System.out.println(cnt);
 	}
 }
