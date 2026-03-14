d41391680e1fd92022d1252b8dfe64a1482e752f
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2026-03-14 23:23:57 +0900

    [Silver II] Title: 색종이 만들기, Time: 100 ms, Memory: 13716 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -4,29 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int white;
-	static int blue;
+	static int[][] arr;
+	static int[] count = new int[2];
 
-	static void check(int[][] paper, int x1, int y1, int x2, int y2) {
-		int color = paper[x1][y1];
-
-		for (int i = x1; i < x2; i++) {
-			for (int j = y1; j < y2; j++) {
-				if (paper[i][j] != color) {
-					check(paper, x1, y1, (x1 + x2) / 2, (y1 + y2) / 2);
-					check(paper, x1, (y1 + y2) / 2, (x1 + x2) / 2, y2);
-					check(paper, (x1 + x2) / 2, y1, x2, (y1 + y2) / 2);
-					check(paper, (x1 + x2) / 2, (y1 + y2) / 2, x2, y2);
+	private static void check(int n, int x, int y) {
+		for (int i = x; i < x + n; i++) {
+			for (int j = y; j < y + n; j++) {
+				if (arr[i][j] != arr[x][y]) {
+					int size = n / 2;
+					
+					for (int dx = 0; dx < 2; dx++) {
+						for (int dy = 0; dy < 2; dy++) {
+							check(size, x + size * dx, y + size * dy);
+						}
+					}
+					
 					return;
 				}
 			}
 		}
 
-		if (color == 0) {
-			white++;
-		} else if (color == 1) {
-			blue++;
-		}
+		count[arr[x][y]]++;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -34,22 +32,18 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		int[][] paper = new int[n][n];
-
+		arr = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			for (int j = 0; j < n; j++) {
-				paper[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		white = 0;
-		blue = 0;
+		check(n, 0, 0);
 
-		check(paper, 0, 0, n, n);
-
-		System.out.println(white);
-		System.out.println(blue);
+		for (int i = 0; i < 2; i++) {
+			System.out.println(count[i]);
+		}
 	}
 }
